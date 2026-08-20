43f3d72600644689d32505188bcc616fc5f0d447
Rohan Sharma <rohan.sharma21@icloud.com>
2026-08-20 09:46:43 +0100

    Update 2_Feature_choices.py

--- pages/2_Feature_choices.py
+++ pages/2_Feature_choices.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import random
 import statsmodels.api as sm
 import streamlit as st
 from plotly.subplots import make_subplots
